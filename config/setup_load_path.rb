d4e5c7ece4ec8ea0b40d9e433de64bb9dd7ac8f3
Kate <k.sunstroke@gmail.com>
2011-07-29 23:51:51 +0400

    вывод видео на стр препода

--- config/setup_load_path.rb
+++ config/setup_load_path.rb
@@ -11,6 +11,9 @@ if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
     raise "RVM ruby lib is currently unavailable."
   end
 end
+require 'rvm'
+RVM.use_from_path! File.dirname(File.dirname(__FILE__))
+RVM.use!('1.8.7@school')
 
 ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
 require 'bundler/setup'

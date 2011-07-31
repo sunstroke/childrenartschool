f0f1f91cd63e30f4de37544bd23ebcf40f0a105f
Kate <k.sunstroke@gmail.com>
2011-07-31 06:22:53 +0400

    welcome slider

---
+++ public/javascripts/init.js
@@ -0,0 +1,35 @@
+jQuery.easing.easeOutQuart = function (x, t, b, c, d) {
+	return -c * ((t=t/d-1)*t*t*t - 1) + b;
+};
+
+jQuery(function( $ ){
+/**
+ * No need to have only one element in view, you can use it for slideshows or similar.
+ * In this case, clicking the images, scrolls to them.
+ * No target in this case, so the selectors are absolute.
+ */
+$('#slider').width($(window).width());
+$(' #buttons').width($(window).width()*0.9);
+$('#slider li, #slider li img').width($('#slider').width());	
+$('#slider ul').width($('#slider').width()*$('#slider li').length);	
+$('#slider').serialScroll({
+	items:'li',
+	prev:'#main a.prev',
+	next:'#main a.next',
+	offset:0, //when scrolling to photo, stop 230 before reaching it (from the left)
+	start:0, //as we are centering it, start at the 2nd
+	duration:1200,
+	force:true,
+	stop:true,
+	lock:false,
+	cycle:true, //don't pull back once you reach the end
+	easing:'easeOutQuart', //use this easing equation for a funny effect
+	jump: true //click on the images to scroll to them
+});
+
+/**
+ * The call below, is just to show that you are not restricted to prev/next buttons
+ * In this case, the plugin will react to a custom event on the container
+ * You can trigger the event from the outside.
+ */
+});

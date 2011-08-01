c13f073eb6e32cf8d66e22ec258e1be4e7bf317f
Kate <k.sunstroke@gmail.com>
2011-08-01 16:36:56 +0400

    сортировка баннеров по полю position

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @banners= Welcome.all(:conditions=>['visible=?',true])
+    @banners= Welcome.all(:conditions=>['visible=?',true], :order=>:position)
   end
 
 end

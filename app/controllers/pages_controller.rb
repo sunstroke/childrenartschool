3ae5dcfb131945bec7b0ad2df099f5a310ba6440
Kate <k.sunstroke@gmail.com>
2011-08-01 14:48:40 +0400

    шаблоны

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,7 +14,7 @@ class PagesController < ApplicationController
   # GET /pages/1.xml
   def show
     @page = Page.find(params[:id])
-    render :layout => 'admin'
+
     # respond_to do |format|
     #   format.html # show.html.erb
     #   format.xml  { render :xml => @page }

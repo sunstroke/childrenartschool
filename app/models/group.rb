9bb7fb7a49808a7c75d408a613c1ba7eef376ad3
Kate <k.sunstroke@gmail.com>
2011-08-04 12:46:01 +0400

    label должны быть уникальны

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,5 @@ class Group < ActiveRecord::Base
   has_many :people, :class_name => "Person" ,:foreign_key => "current_group"
   belongs_to :teacher
   validates_format_of :label, :with =>/^[a-zA-Z-0-9]+$/x, :message => 'Ссылка должна содержать только строчные латинские буквы или цифры'    
+  validates_uniqueness_of :label, :message => "должно быть уникальным"
 end

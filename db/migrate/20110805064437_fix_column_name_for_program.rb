300e50894569935bed8fbed4a84b96e5d78d46ed
Kate <k.sunstroke@gmail.com>
2011-08-05 10:57:10 +0400

    страница школы

---
+++ db/migrate/20110805064437_fix_column_name_for_program.rb
@@ -0,0 +1,11 @@
+class FixColumnNameForProgram < ActiveRecord::Migration
+  def self.up
+    rename_column :programs, :type, :program_type
+    rename_column :people, :type, :role_type    
+  end
+
+  def self.down
+    rename_column :programs, :program_type, :type
+    rename_column :people, :role_type, :type
+  end
+end

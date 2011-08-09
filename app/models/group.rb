bc66d19af4a466c21948b60afa6148eb5b4a5619
Kate <k.sunstroke@gmail.com>
2011-08-09 16:10:01 +0400

    группа и препод должны быть выбраны

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,6 @@ class Group < ActiveRecord::Base
   belongs_to :teacher
   validates_format_of :label, :with =>/^[a-zA-Z-0-9]+$/x, :message => 'Ссылка должна содержать только строчные латинские буквы или цифры'    
   validates_uniqueness_of :label, :message => "должно быть уникальным"
+  validates_presence_of :teacher_id, :message => "должен быть указан"
+  validates_presence_of :program_id, :message => "должена быть указана"  
 end

ab2fdb5cbb84bfdc5c291634a822abc373355893
Kate <k.sunstroke@gmail.com>
2011-08-10 22:12:21 +0400

    в списке учеников доп инфа

--- app/helpers/admin/people_helper.rb
+++ app/helpers/admin/people_helper.rb
@@ -1,2 +1,6 @@
 module Admin::PeopleHelper
+  def age(birthday)
+    @age=Time.now.utc.to_date.year - birthday.to_date.year - (birthday.to_date.change(:year => Time.now.utc.to_date.year) > Time.now.utc.to_date ? 1 : 0)
+    @age.to_s
+  end
 end

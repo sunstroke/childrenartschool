6d5e51fecd1c9c1a16792efd9831c2952c192776
Kate <k.sunstroke@gmail.com>
2011-08-11 03:25:10 +0400

    уведомление о новой заявке

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default :from => "robot@school-f.ru"
   def welcome_email(user)
     @user = user
-    mail(:to => "k.sunstroke@gmail.com",
-         :subject => "Новая заявка #{@user.fullname}")
+    mail(:to => "k.sunstroke@gmail.com,ekaterina.motyleva@school-f.ru,children@school-f.ru",
+         :subject => "Новая заявка в группу #{@user.group.name}")
   end  
 end

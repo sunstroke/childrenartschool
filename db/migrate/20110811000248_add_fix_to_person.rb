f86b7181219ca60945f3db5e56497d676076d08b
Kate <k.sunstroke@gmail.com>
2011-08-11 04:12:02 +0400

    подписка, по дефолту подписаны

---
+++ db/migrate/20110811000248_add_fix_to_person.rb
@@ -0,0 +1,12 @@
+class AddFixToPerson < ActiveRecord::Migration
+  def self.up
+      change_column :people, :sub_all, :boolean, :default => true
+      change_column :people, :sub_group, :boolean, :default => true          
+      Person.update_all ["sub_all = ?", true]
+      Person.update_all ["sub_group = ?", true]        
+  end
+
+  def self.down
+    
+  end
+end

d31c87123eb115d81a26d247dd785d4ee6032144
Kate <k.sunstroke@gmail.com>
2011-09-11 15:51:50 +0400

    news to old

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,35 @@
 Schoolf::Application.routes.draw do
+  
+  # Omniauth pure
+    match "/signin" => "services#signin"
+    match "/signout" => "services#signout"
+
+    match '/auth/:service/callback' => 'services#create' 
+    match '/auth/failure' => 'services#failure'
+
+    resources :services, :only => [:index, :create, :destroy] do
+      collection do
+        get 'signin'
+        get 'signout'
+        get 'signup'
+        post 'newaccount'
+        get 'failure'
+      end
+    end
+
+    # used for the demo application only
+    resources :users, :only => [:index] do
+      collection do
+        get 'test'
+      end
+    end
+
+    # root :to => "users#index"
+  
   # Sample resource route within a namespace:
 match '/teachers' , :to => redirect("http://2010.school-f.ru/teachers")
 match '/photocourses' , :to => redirect("http://2010.school-f.ru/photocourses")
-
+match '/news' , :to => redirect("http://2010.school-f.ru/news")
 namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)

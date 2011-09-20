37f76386aa7a337fd44e6901d2a5a0ddb2e09421
Kate <k.sunstroke@gmail.com>
2011-09-20 20:54:31 +0400

    убрать из группы тех кто в архиве

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
   belongs_to :program
-  has_many :people, :class_name => "Person" ,:foreign_key => "current_group"
+  has_many :people, :class_name => "Person" ,:foreign_key => "current_group", :conditions=>["failure = ?",false]
   belongs_to :teacher
   validates_format_of :label, :with =>/^[a-zA-Z-0-9]+$/x, :message => 'Ссылка должна содержать только строчные латинские буквы или цифры'    
   validates_uniqueness_of :label, :message => "должно быть уникальным"

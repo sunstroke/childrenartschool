4edf26df3419b8b8c11f1aea16421e0a2e0fe0f4
Kate <k.sunstroke@gmail.com>
2012-04-02 10:30:01 +0400

    regexp

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class CkeditorPictureUploader < CarrierWave::Uploader::Base
   include Ckeditor::Backend::CarrierWave
-  # CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+CarrierWave::SanitizedFile.sanitize_regexp = /[^a-zA-Zа-яА-ЯёЁ0-9\.\-\+_]/u
   # Include RMagick or ImageScience support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
@@ -44,11 +44,11 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   def extension_white_list
     Ckeditor.image_file_types
   end
-  #  def filename
-  #       ext = File.extname(original_filename) if original_filename
-  #       name = File.basename(original_filename, '.*') if original_filename
-  #       "#{name.parameterize}#{ext}" if original_filename
-  # end
+   def filename
+        ext = File.extname(original_filename) if original_filename
+        name = File.basename(original_filename, '.*') if original_filename
+        "#{name.parameterize}#{ext}" if original_filename
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.

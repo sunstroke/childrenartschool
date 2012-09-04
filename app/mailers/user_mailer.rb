6ff04aa64202bbc5059e473c4890ed6e4389c3d5
Kate <k.sunstroke@gmail.com>
2012-09-04 15:19:52 +0400

    mail list

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   default :from => "robot@school-f.ru"
   def welcome_email(user)
     @user = user
-    mail(:to => "k.sunstroke@gmail.com, ekaterina.motyleva@school-f.ru, children@school-f.ru, victoriya.1801@gmail.com",
+    mail(:to => "k.sunstroke@gmail.com, alexandra@school-f.ru, alexey.tkachev@school-f.ru,  mashafoto@mail.ru ",
          :subject => "Новая заявка в группу #{@user.group.name}")
   end  
   def reset_password_email(user)

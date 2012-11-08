bcda2e6d140a13cd1e2dfd0230bcdff5faa88fb0
Kate <k.sunstroke@gmail.com>
2012-11-09 01:50:31 +0400

    add tel

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,8 +3,8 @@ class Person < ActiveRecord::Base
     audited
   attr_accessible   :fullname, :role_type, :zachetka, :tel, :birthday, :about, :current_group, :mail, :sub_all, :sub_group, :failure
   belongs_to :group, :class_name => "Group" ,:foreign_key => "current_group"
-  validates_presence_of :tel,  :message => "не может быть пустым"
-  validates_presence_of :current_group,  :message => "должна быть выбрана"
+  # validates_presence_of :tel,  :message => "не может быть пустым"
+  # validates_presence_of :current_group,  :message => "должна быть выбрана"
   #validates_format_of :mail, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i , :message => "не верный формат"
   
 end

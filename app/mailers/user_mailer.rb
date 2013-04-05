e3989af74b840e8c056444b4a9715620e1671a0f
Kate <k.sunstroke@gmail.com>
2013-04-05 23:36:57 +0400

    удалила лишних из рассылки, поправила смарт шаблон

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   default :from => "robot@school-f.ru"
   def welcome_email(user)
     @user = user
-    mail(:to => "k.sunstroke@gmail.com, alexandra@school-f.ru, alexey.tkachev@school-f.ru,  mashafoto@mail.ru ",
+    mail(:to => "k.sunstroke@gmail.com,  alexey.tkachev@school-f.ru ",
          :subject => "Новая заявка в группу #{@user.group.name if @user.group}")
   end  
   def reset_password_email(user)

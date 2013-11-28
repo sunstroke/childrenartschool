8734ccf632e3a731120a6ba09f75c78cdb55d54f
Kate <k.sunstroke@gmail.com>
2013-11-28 21:07:43 +0400

    js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
+
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 var redactor = { 
